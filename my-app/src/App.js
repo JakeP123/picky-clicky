16ef29eb501ad71511add8f45aafb468de473fe1
Jacob Phillips <phillips.jakecolby@gmail.com>
2019-08-02 10:07:01 -0400

    working version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,54 +3,96 @@ import logo from './logo.svg';
 import './App.css';
 import data from './data'
 
- class App extends React.Component {
+class App extends React.Component {
   state = {
-    data, 
+    data,
     score: 0,
-    wasClicked: []
+    wasClicked: [],
+    topScore: 0,
+    messages: ["You've Guessed Correctly!", "You've Guessed Incorrectly!"],
+    message: "Click an image to begin!"
   }
   renderImages = () => {
-    return this.state.data.map(item => 
+    return this.state.data.map(item =>
       <img className="card" src={item.URL} value={item.ID} onClick={this.handleOnClick}></img>
     )
   }
 
+  shuffleCards = () => {
+    this.state.data.sort(() => 
+    
+      .5-Math.random()
+    )
+  }
   handleOnClick = e => {
-const ID = e.currentTarget.getAttribute("value")
-if (this.state.wasClicked.includes(ID)) {
-console.log("You've Clicked This Dummy!")
-{this.handleDecrement()}
-} else {
-  this.setState({
-    wasClicked: this.state.wasClicked.concat(ID)
-  })
-  console.log(this.state.wasClicked)
-  {this.handleIncrement()}
-}
-   
+    const ID = e.currentTarget.getAttribute("value")
+    if (this.state.wasClicked.includes(ID)) {
+      console.log("You've Clicked This Dummy!")
+      { this.handleDecrement() }
+    } else {
+      this.shuffleCards()
+      this.setState({
+        wasClicked: this.state.wasClicked.concat(ID)
+      })
+      console.log(this.state.wasClicked)
+      { this.handleIncrement() }
+    }
+
   }
 
   handleIncrement = () => {
+
     this.setState({
-      score: this.state.score +1
+      score: this.state.score + 1
+    })
+    this.setState({
+      message: this.state.messages[0]
     })
   }
-  
+
   handleDecrement = () => {
+    if (this.state.score > this.state.topScore) {
+      this.setState({
+        topScore: this.state.score
+      })
+    }
     this.setState({
-      score: this.state.score -1
+      score: 0
+    })
+    this.setState({
+      message: this.state.messages[1]
     })
   }
-   render() {
-   console.log(this.state.data)
+  render() {
+    console.log(this.state.data)
     return (
+
+      //HTML for main page
       <div className="App">
-        <h1>Your Score: {this.state.score}</h1>
-    {this.renderImages()}  
+
+        <div className="header">
+
+          <img className="donut" src="https://i.imgur.com/R2onQ95.png"></img>
+          <h4 className="top first">Simpson's Game</h4>
+          <h4 className="top middle">{this.state.message}</h4>
+          <h4 className="top">Your Score: {this.state.score}  | Top Score: {this.state.topScore}</h4>
+        </div>
+
+        <div className="instructions">
+          <h1 className="info">THE SIMPSONS</h1>
+          <h4 className="info">Memory Click Game!</h4>
+          <h6 className="info">Click an image to earn points, but don't click on any image more than once!</h6>
+        </div>
+        <div className="game">
+          <div className="images">
+          {this.renderImages()}
+          </div>
+        </div>
       </div>
+   
     );
-   }
- 
+  }
+
 }
 
 export default App;
--- my-app/src/data.js
+++ my-app/src/data.js
@@ -1,11 +1,67 @@
 const data = [
     {
-        URL: "https://i.imgur.com/OXntOZG.png",
+        URL: "https://i.imgur.com/HkCyDAs.jpg",
         ID: 1
     },
     {
-        URL: "https://i.imgur.com/GYiupaZ.png",
+        URL: "https://i.imgur.com/QLLTDpo.jpg",
         ID: 2
+    },
+    {
+        URL: "https://i.imgur.com/z47HwrH.jpg",
+        ID: 3
+    },
+    {
+        URL: "https://i.imgur.com/ESKHPyJ.jpg",
+        ID: 4
+    },
+    {
+        URL: "https://i.imgur.com/0zV4Yn2.jpg",
+        ID: 5
+    },
+    {
+        URL: "https://i.imgur.com/GaIUJXK.jpg",
+        ID: 6
+    },
+    {
+        URL:"https://i.imgur.com/uCkIPLe.jpg",
+        ID: 7
+    },
+    {
+        URL: "https://i.imgur.com/MxDvqpg.jpg",
+        ID: 8
+    },
+    {
+        URL: "https://i.imgur.com/V7FHoUy.jpg",
+        ID: 9
+    },
+    {
+        URL: "https://i.imgur.com/NHQeupv.jpg",
+        ID: 10
+    },
+    {
+        URL: "https://i.imgur.com/nPE5Grk.jpg",
+        ID: 11
+    },
+    {
+        URL: "https://i.imgur.com/gCM4rVw.jpg",
+        ID: 12
+    },
+    {
+        URL: "https://i.imgur.com/kxfOv1Z.jpg",
+        ID: 13
+    },
+    {
+        URL: "https://i.imgur.com/OdFiHlx.jpg",
+        ID: 14
+    },
+    {
+        URL: "https://i.imgur.com/f5CCXv7.jpg",
+        ID: 15
+    },
+    {
+        URL: "https://i.imgur.com/QwnnCJO.jpg",
+        ID: 16
     }
 ]
 
